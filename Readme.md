d7e5ad58831b0ee253925b23f79b90c4bfccafc9
elemperador <juliobolivar@gmail.com>
2016-10-10 09:22:23 +0200

    Assignment

---
+++ Readme.md
@@ -0,0 +1,23 @@
+
+# Getting and Cleaning Data
+
+## Course Project
+
+You should create one R script called run_analysis.R that does the following.
+
+1. Merges the training and the test sets to create one data set.
+2. Extracts only the measurements on the mean and standard deviation for each measurement.
+3. Uses descriptive activity names to name the activities in the data set
+4. Appropriately labels the data set with descriptive activity names.
+5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## Steps to work on this course project
+
+1. Download the files in a local folder.
+2. Copy ```run_analysis.R``` in the selected folder.
+3. Set the directory as your working directory.
+4. Run ```source("run_analysis.R")```.
+
+## Dependencies
+
+```dplyr``` and ```data.table```. 
---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+## Create one R script called run_analysis.R that does the following:
+## 1. Merges the training and the test sets to create one data set.
+## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3. Uses descriptive activity names to name the activities in the data set
+## 4. Appropriately labels the data set with descriptive activity names.
+## 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# Load required packages
+require("data.table")
+require("dplyr")
+
+# Load data
+labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
+features <- read.table("./UCI HAR Dataset/features.txt")[,2]
+
+testSetX <- read.table("./UCI HAR Dataset/test/X_test.txt")
+testSetY <- read.table("./UCI HAR Dataset/test/y_test.txt")
+subjectTest <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+testSetY[,2] <- labels[testSetY[,1]]
+names(testSetY) <- c("ID", "Name")
+names(subjectTest) <- "subject"
+
+trainSetX <- read.table("./UCI HAR Dataset/train/X_train.txt")
+trainSetY <- read.table("./UCI HAR Dataset/train/y_train.txt")
+subjectTrain <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+trainSetY[,2] = labels[trainSetY[,1]]
+names(trainSetY) = c("ID", "Name")
+names(subjectTrain) = "subject"
+
+# Name sets
+names(testSetX) <- features
+names(trainSetX) <- features
+
+# Extract Information
+measurementsIndex <- grep("mean|std", features)
+testSetX <- testSetX[,measurementsIndex]
+trainSetX = trainSetX[,measurementsIndex]
+
+# Merge Data
+mergedData <- rbind(cbind(subjectTest, testSetY, testSetX), 
+             cbind(subjectTrain, trainSetY, trainSetX))
+
+# Create new dataset
+dataDplr <- tbl_df(mergedData)
+
+# Calculate Means and store in indepedent tidy dataset
+tidyDataSet <- dataDplr %>% group_by(Name) %>% summarise_each(funs(mean))
+
